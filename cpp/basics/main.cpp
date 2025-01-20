0fed20b7abb84e5d2e7165f9d1fcaa0c20048596
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2025-01-20 05:42:45 +0530

    learning c++

---
+++ cpp/basics/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    int sum;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Sum: " << sum << endl;
+    return 0;
+}
