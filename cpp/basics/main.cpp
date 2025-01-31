50404b9a017232ad62fbb7934a4d26a792180939
dilshad-ahmad-actyv <dilshadsoraon09@gmail.com>
2025-01-31 09:16:44 +0530

    c++ basic course got over

--- cpp/basics/main.cpp
+++ cpp/basics/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
-    int x, y;
-    int sum;
-    cout << "Enter a number: ";
-    cin >> x;
-    cout << "Type another number: ";
-    cin >> y;
-    sum = x + y;
-    cout << "Sum: " << sum << endl;
+    srand(time(0));
+    int myRandom = rand() % 11;
+    cout << "Random number: " << myRandom << endl;
     return 0;
 }
