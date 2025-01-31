acced3804a352d8ac61983c86c5532bea322833e
dilshad-ahmad-actyv <dilshadsoraon09@gmail.com>
2025-01-31 09:18:11 +0530

    python tutorials is in progress

--- Python/index.py
+++ Python/index.py
@@ -110,17 +110,70 @@
 # print(student1)
 
 
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+# class Person:
+#   def __init__(self, fname, lname):
+#     self.firstname = fname
+#     self.lastname = lname
 
-  def printname(self):
-    print(self.firstname, self.lastname)
+#   def printname(self):
+#     print(self.firstname, self.lastname)
 
-class Student(Person):
-  def __init__(self, fname, lname):
-     Person.__init__(self, fname, lname)
+# class Student(Person):
+#   def __init__(self, fname, lname):
+#      Person.__init__(self, fname, lname)
      
-x = Student("Mike", "Olsen")
-x.printname()
+# x = Student("Mike", "Olsen")
+# x.printname()
+
+
+# arr = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]]]
+
+# res = {n for depth_1 in arr for depth_2 in depth_1 for n in depth_2}
+# print(res)
+
+# mytuple = ("apple", "banana", "cherry")
+
+# myIter = iter(mytuple)
+
+# for i in range(len(mytuple)):
+#     print(next(myIter))
+
+# class MyNumbers:
+#     def __init__(self, nums):
+#         self.nums = nums
+#         self.index = 0
+    
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+#         if self.index >= len(self.nums):
+#             raise StopIteration()
+#         result = self.nums[self.index]
+#         self.index += 1
+#         return result
+    
+# nums = [13, 23, 28, 20, 30, 40]
+# myNumbers = MyNumbers(nums)
+# myIter = iter(myNumbers)
+
+# for x in myIter:
+#     print(x)
+
+# def my_generator():
+#     for n in range(10):
+#         yield n
+    
+#     def __str__(self):
+#         return self.n
+        
+# # for x in my_generator():
+# #     print(x)
+# x = my_generator()
+# print(x)
+
+data = [1, 2, 3, 4]
+
+
+squares = map( lambda x: x ** 2, data)
+print(list(squares))
